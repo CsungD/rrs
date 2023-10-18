3f28c867136e93be3437d7f3098b87cb2f4f709e
CsungD <csungd93@gmail.com>
2023-10-18 13:15:29 +0900

    start commit

---
+++ src/main/java/com/portfolio/untitled/UntitledApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.untitled;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UntitledApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UntitledApplication.class, args);
+	}
+
+}
